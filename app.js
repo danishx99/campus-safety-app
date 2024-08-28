47c0bd40fc43e06d64dc8412f26da56023f39f08
KatangaDan <katangadan680@gmail.com>
2024-08-28 15:11:38 +0200

    chore: Update dependencies and server configuration

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path= require('path')     // so we can render the frontend
+const app = express()
+const PORT = 3000
+
+app.use(express.json())         // automatically passes body as json object
+app.use(express.static(path.join(__dirname, 'client/html')))     // server static files from 'public' directory 
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+  res.send(path.join(__dirname,'client/html','index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+})

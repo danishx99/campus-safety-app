db816f2edcb3fc31a5eb2e6bf0b1734081eac3c4
KatangaDan <katangadan680@gmail.com>
2024-08-28 20:34:01 +0200

    chore: Add static file serving for styles directory
    ```

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const PORT = 3000
 
 app.use(express.json())         // automatically passes body as json object
 app.use(express.static(path.join(__dirname, 'client/html')))     // server static files from 'public' directory 
+app.use(express.static(path.join(__dirname, 'client/styles')));     // serve static files from 'styles' directory
 app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
   res.send(path.join(__dirname,'client/html','index.html'));
 })
+
+app.get('/login', (req, res) => {
+    res.send(path.join(__dirname,'client/html/auth','login.html'));
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);

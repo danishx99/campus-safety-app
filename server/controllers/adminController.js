0ddea7e478bfe8fbf807f8d56fed896f0c5e4713
KhaalidTyer <2576429@students.wits.ac.za>
2024-09-05 17:38:29 +0200

    KHAALID admin js

---
+++ server/controllers/adminController.js
@@ -0,0 +1,9 @@
+// here we do all our dodgy stuff
+const bcrypt = require("bcryptjs"); // For password hashing
+const jwt = require("jsonwebtoken"); // For generating JSON Web Tokens
+const dotenv = require("dotenv"); // For accessing environment variables
+
+exports.adminSafetyResources = async (req,res) =>{
+
+};
+

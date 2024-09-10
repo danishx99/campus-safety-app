f14a0a9213e0334b3030573205c9e5a6a0776e49
ThendoRavi <thendorav@gmail.com>
2024-09-10 19:20:38 +0200

    feat: Add Google sign-up option to registration form

--- client/javascript/registerGoogle.js
+++ client/javascript/registerGoogle.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    var googleBtn= document.getElementById("continue-google-btn");
+    var continueGoogle= document.getElementById("continue-google-btn");
+    var proceedGoogle = document.getElementById("proceed-google-btn");
+
+    var form1 = document.getElementById("form-p1");
+    var form2 = document.getElementById("form-p2");
 
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,17 +20,23 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Initialize Firebase
     // initializeApp(firebaseConfig);
+
+    continueGoogle.addEventListener("click", function (event) {
+        event.preventDefault();
+        form1.className = "hidden";
+        form2.className = "block p-2 space-y-6";
+        //submitButton.style.display = "none";
+        //registerWithGoogle.style.display = "flex";
+    });
     
 
-    googleBtn.addEventListener("click", function(event){
+    proceedGoogle.addEventListener("click", function(event){
 
         event.preventDefault();
 
         alert("Sign up with Google btn clicked");
-
-
        
-    })
+    });
     
 
 })

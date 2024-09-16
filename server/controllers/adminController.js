65e7aef0edd37a3a4794108422644608641a89e5
KhaalidTyer <2576429@students.wits.ac.za>
2024-09-16 23:33:10 +0200

    khaalid tyer

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,7 +1,3 @@
-// here we do all our dodgy stuff
-const bcrypt = require("bcryptjs"); // For password hashing
-const jwt = require("jsonwebtoken"); // For generating JSON Web Tokens
-const dotenv = require("dotenv"); // For accessing environment variables
 const safetyResources= require("../schemas/safetyResources")
 const ObjectId = require('mongodb').ObjectId;
 

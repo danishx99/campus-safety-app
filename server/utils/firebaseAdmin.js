56735c695896cfe570c43688c562f222384e16ea
Danish Saleem <saleemdf99@gmail.com>
2024-09-17 13:24:19 +0200

    .

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -1,20 +1,32 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../campus-safety-fcm-firebase-adminsdk-k3uhg-5fe8524d8f.json');
+require("dotenv").config(); // Load environment variables from .env file
 
-//Initialize the admin SDK with the service account and make sure to catch any errors
+const admin = require("firebase-admin");
+
+// Construct service account object from environment variables
+const serviceAccount = {
+  type: "service_account",
+  project_id: "campus-safety-fcm",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle newline characters in private key
+  client_email:
+    "firebase-adminsdk-k3uhg@campus-safety-fcm.iam.gserviceaccount.com",
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-k3uhg%40campus-safety-fcm.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
 
 try {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
-
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 } catch (error) {
-    console.log('Firebase admin initialization error: ', error);
+  console.log("Firebase admin initialization error: ", error);
 }
 
-
-
-// //Log to see if the admin is initialized(by finding out from admin)
-// console.log(admin);
+//console.log(admin);
 
 module.exports = admin;

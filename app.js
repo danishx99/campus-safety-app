c11a4e96b3fac158c95247ba16abd3ed703f153d
DelanoMarcell <delanomartin14@gmail.com>
2024-10-16 11:52:51 +0200

    Refactor package.json to add cheerio and compression dependencies. Added lazy loading of images by intercepting response body.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
   const express = require("./server/node_modules/express");
   const mongoose = require("./server/node_modules/mongoose");
+  const cheerio = require("./server/node_modules/cheerio");
+  const compression = require("./server/node_modules/compression");
   const cookieParser = require("./server/node_modules/cookie-parser");
   const bodyParser = require("./server/node_modules/body-parser");
   const path = require("path");
@@ -9,6 +11,10 @@
   const sendNotification = require("./server/utils/sendNotification.js");
   const app = express();
   const PORT = process.env.PORT || 3000;
+
+
+  //Add GZIP compression to improve performance,the compression middleware compresses the response bodies for all requests that pass through it.
+  app.use(compression());
 
   // const cron = require('./server/node_modules/node-cron');
 
@@ -36,8 +42,35 @@
   app.use(cookieParser()); // Parse cookies
   // Set a larger limit for JSON bodies
 
-  // Serve frontend static files from the 'client' directory
-  app.use(express.static("client"));
+  // Serve frontend static files from the 'client' directory, add cache control so that the browser caches the files for 1 day
+  app.use(express.static('client', {
+    maxAge: '1d', // Cache for 1 day
+    etag: false
+  }));
+  
+  // Middleware to add lazy loading to all <img> tags
+app.use((req, res, next) => {
+  const originalSend = res.send;
+
+  res.send = function (body) {
+    // Check if the body is a string and the response is HTML
+    if (typeof body === 'string' && this.get('Content-Type')?.includes('text/html')) {
+      // Load the body into Cheerio
+      const $ = cheerio.load(body);
+      
+      // Add 'loading="lazy"' to all <img> tags that don't already have a 'loading' attribute
+      $('img:not([loading])').attr('loading', 'lazy');
+
+      // Serialize the modified HTML
+      body = $.html();
+    }
+
+    // Send the modified body
+    originalSend.call(this, body);
+  };
+
+  next();
+});
 
   // Route middleware
   app.use("", frontendRoutes);
